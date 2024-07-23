7700d2a08e0badcf5a17555c46c29c2d10141346
Bala-code-24 <balajisanthanam200@gmail.com>
2024-07-23 18:42:18 +0530

    hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,36 +1,29 @@
-import HeroImg from '../assests/hero.png';
+
+import HeroImg from '../assets/hero.png';
 import { AiOutlineTwitter, AiOutlineInstagram, AiOutlineLinkedin } from "react-icons/ai";
 
 export default function Hero() {
     const config  = {
-        subtitle: 'Im a Full-stack developer,IOT Specialist and 3D Model Designer',
+        subtitle: 'Im a Full-stack developer,IOT specialist and 3d Model Designer',
         social: {
-            twitter: '',
-            facebook: '',
-            linkedin: ''
+            twitter: 'https://twitter.com/jvlcode',
+            facebook: 'https://facebook.com/jvlcode',
+            linkedin: 'https://in.linkedin.com/company/jvl-code'
         }
     }
 
 
     return <section className='flex flex-col md:flex-row px-5 py-32 bg-primary justify-center'>
        <div className='md:w-1/2 flex flex-col'>
-<<<<<<< HEAD
-            <h1 className=' text-white text-6xl font-hero-font'>Hi, <br/> Im <span className=''>BALAJI</span> HERE
-=======
-            <h1 className=' text-white text-6xl font-hero-font'>Hi, <br/> Im <span className='text-white'>BALAJI</span> HERE
->>>>>>> 1dfd800b6819d4577b0f7cf2adc70b758503ed9a
+            <h1 className=' text-white text-6xl font-hero-font'>Hi, <br/> Im <span className='text-black'>Balaji</span> here
                 <p className='text-2xl'>{config.subtitle}</p>
             </h1>
             <div className='flex py-10'>
                 <a href={config.social.twitter} className='pr-5 hover:text-white'><AiOutlineTwitter size={40} /></a>
-                <a href={config.social.facebook} className='pr-5 hover:text-white'><AiOutlineInstagram size={40}/></a>
+                <a href={config.social.AiOutlineInstagram} className='pr-5 hover:text-white'><AiOutlineFacebook size={40}/></a>
                 <a href={config.social.linkedin} className='hover:text-white'><AiOutlineLinkedin size={40}/></a>
             </div>
        </div>
-<<<<<<< HEAD
-       <img alt="hero.png" className='md:w-1/3' src={HeroImg} />
-=======
-       <img src="" className='md:w-1/3' src={HeroImg} />
->>>>>>> 1dfd800b6819d4577b0f7cf2adc70b758503ed9a
+       <img alt=" " className='md:w-1/3' src={HeroImg} />
     </section>
 }
